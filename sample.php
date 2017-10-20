a09e8bb37fde3937b39454ec59dba7e7b38bd0d3
gellai <32382614+gellai@users.noreply.github.com>
2017-10-20 09:26:09 +0100

    Update sample.php

--- sample.php
+++ sample.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
     <header></header>
     <body>
-        <p><img src=classes/GellaiCaptcha.php?mode=raw&length=10&lColor=d40000 /></p>
+        <p><img src=GellaiCaptcha.php?mode=raw&length=10&lColor=d40000&tColor=d40000&bColor=d09898 /></p>
         <?php $param = array('length' => 8); ?>
         
         <p><?php echo $gCaptcha->getCaptcha($param); ?></p>
