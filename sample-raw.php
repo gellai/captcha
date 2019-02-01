6ce954902fe420936f85e2c656363460e001c741
gellai <32382614+gellai@users.noreply.github.com>
2019-02-01 12:45:26 +0000

    Add files via upload

---
+++ sample-raw.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+    <header></header>
+    <body>
+        <h1>Raw Mode</h1>
+        <p><img src="GellaiCaptcha.php?mode=raw&length=10&lColor=d40000&tColor=d40000&bColor=d09898" /></p>
+    </body>
+</html>
